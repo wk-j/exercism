c5c5fb4035267c02f65a18a014e01fc7aca73607
wk-j <somnuk.wk@gmail.com>
2017-10-14 17:22:56 +0700

    Initial commit

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,13 @@
+﻿module Leap
+
+let (|DivBy|_|) divisor i  =
+    if i % divisor = 0 
+        then Some()
+    else None
+
+let leapYear (year: int): bool =
+    match year with
+    | DivBy 100 | DivBy 400 -> false
+    | DivBy 4 -> true
+    | _ -> false
+
