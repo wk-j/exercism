0c271c607ea8b377938d08a676074ef464b38bc0
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-10-16 20:33:50 +0700

    Bob

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,3 +1,49 @@
 ﻿module Bob
+open System
 
-let response (input: string): string = failwith "You need to implement this function."
+let isSome = function
+    | true -> Some()
+    | false -> None
+
+let toChar (input: string) = 
+    input.Trim().ToCharArray() |> Seq.filter(fun x -> x <> ' ')
+
+let (|Yell|_|) (input: string) = 
+    input 
+    |> toChar
+    |> Seq.forall (fun x -> (Char.IsUpper(x)  ||  
+                                (Char.IsDigit(x) 
+                                  || x = '!' 
+                                  || x = '?' 
+                                  || Char.IsLetterOrDigit(x) |> not
+                                  || x = ',')))
+    |> isSome
+
+let (|Number|_|) (input:string) = 
+    input 
+    |> toChar
+    |> Seq.forall(fun x -> Char.IsNumber(x) || x = ',')
+    |> isSome
+
+let (|Forceful|_|) (input: string) = 
+    input 
+    |> toChar
+    |> Seq.forall(fun x -> Char.IsUpper(x) || x = '?')
+    |> isSome
+
+let (|Silence|_|) (input: string) =
+    input.Trim() |> (=) "" |> isSome
+
+let (|Question|_|) (input: string) = 
+    input 
+    |> toChar
+    |> Seq.last |> (=) '?' |> isSome
+
+let response (input: string): string = 
+    match input with
+    | Silence ->  "Fine. Be that way!"
+    | Forceful -> "Whoa, chill out!"
+    | Number -> "Whatever."
+    | Question -> "Sure."
+    | Yell -> "Whoa, chill out!"
+    | _ -> "Whatever."
